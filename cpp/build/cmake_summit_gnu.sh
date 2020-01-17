bb047d4ae52dc3a6467860504fffd993cc33168d
Matt Norman <normanmr@ornl.gov>
2020-01-17 11:07:35 -0500

    CMake working for mpi and serial for C++ code

---
+++ cpp/build/cmake_summit_gnu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##############################################################################
+## This requires gcc/8.1.1 on Summit right now
+##############################################################################
+
+./cmake_clean.sh
+
+cmake -DCMAKE_CXX_COMPILER=mpic++                   \
+      -DPNETCDF_PATH=${OLCF_PARALLEL_NETCDF_ROOT}   \
+      -DCXXFLAGS="-O3"                              \
+      -DLDFLAGS=""                                  \
+      ..
+
