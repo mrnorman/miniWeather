1fd9c667fa05451275ca2cbae0746e7d5efbe55b
Matt Norman <normanmr@ornl.gov>
2020-07-28 12:47:21 -0400

    Adding a cmake file for my sm50 GPU laptop, fhqwhgads

---
+++ cpp/build/cmake_fhqwhgads_gnu_gpu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+./cmake_clean.sh
+
+cmake -DCMAKE_CXX_COMPILER=mpic++                   \
+      -DPNETCDF_PATH=${PNETCDF_PATH}                \
+      -DYAKL_CUB_HOME=`pwd`/../cub                  \
+      -DCXXFLAGS="-O3 -std=c++14"                   \
+      -DCUDA_FLAGS="-arch sm_50 -ccbin mpic++"      \
+      -DNX=200                                      \
+      -DNZ=100                                      \
+      -DSIM_TIME=5                                  \
+      -DARCH="CUDA"                                 \
+      ..
+
