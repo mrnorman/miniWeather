c59de7aa4f6684b3af29bf33bb518e9ade7039d2
Matt Norman <normanmr@ornl.gov>
2021-02-01 12:57:22 -0500

    Upgrading to the latest YAKL

--- cpp/build/cmake_fhqwhgads_gnu_gpu.sh
+++ cpp/build/cmake_fhqwhgads_gnu_gpu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export TEST_MPI_COMMAND="mpirun -n 1"
+unset CUDAFLAGS
 
 ./cmake_clean.sh
 
 cmake -DCMAKE_CXX_COMPILER=mpic++                   \
       -DPNETCDF_PATH=${PNETCDF_PATH}                \
-      -DYAKL_CUB_HOME=`pwd`/../cub                  \
       -DCXXFLAGS="-O3 -std=c++11"                   \
       -DCUDA_FLAGS="-arch sm_50 -ccbin mpic++"      \
       -DNX=200                                      \
