8dd632f82b8d7484afb3705e5021b61b06c57996
Matt Norman <normanmr@ornl.gov>
2022-02-04 17:29:35 -0500

    Continuing to look at compilers for C++

--- cpp/build/cmake_fhqwhgads_intel_cpu.sh
+++ cpp/build/cmake_fhqwhgads_intel_cpu.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+source /usr/share/modules/init/bash
+module purge
+
 export TEST_MPI_COMMAND="mpirun -n 1"
 unset CUDAFLAGS
 unset CXXFLAGS
 
 export OMPI_CXX=/opt/intel/oneapi/compiler/2022.0.2/linux/bin/icpx
 export OMPI_CC=/opt/intel/oneapi/compiler/2022.0.2/linux/bin-llvm/clang
-export OMPI_F90=gfortran
+export OMPI_FC=/opt/intel/oneapi/compiler/2022.0.2/linux/bin/intel64/ifort
+
+export LD_LIBRARY_PATH=/opt/intel/oneapi/compiler/2022.0.2/linux/compiler/lib/intel64_lin:$LD_LIBRARY_PATH
 
 ./cmake_clean.sh
 
@@ -14,7 +19,7 @@ cmake -DCMAKE_CXX_COMPILER=mpic++         \
       -DCMAKE_C_COMPILER=mpicc            \
       -DCMAKE_Fortran_COMPILER=mpif90     \
       -DPNETCDF_PATH=${PNETCDF_PATH}      \
-      -DYAKL_CXX_FLAGS="-Ofast -std=c++11 -fno-alias -DNO_INFORM"   \
+      -DYAKL_CXX_FLAGS="-Ofast -std=c++11 -DNO_INFORM"   \
       -DNX=200                            \
       -DNZ=100                            \
       -DSIM_TIME=1000                     \
