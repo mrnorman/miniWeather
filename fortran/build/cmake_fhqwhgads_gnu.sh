b6819a9c053cf885c5b933527603cfe1b50833ff
Matt Norman <normanmr@ornl.gov>
2022-07-06 10:25:57 -0400

    Simplifying cmake system to make it more flexible. Updating fhqwhgads and summit machine files

--- fortran/build/cmake_fhqwhgads_gnu.sh
+++ fortran/build/cmake_fhqwhgads_gnu.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 export TEST_MPI_COMMAND="mpirun -n 1"
+export OMPI_FC=gfortran-11
 
 ./cmake_clean.sh
 
-cmake -DCMAKE_Fortran_COMPILER=mpif90               \
-      -DPNETCDF_PATH=${PNETCDF_PATH}   \
-      -DOPENMP_FLAGS="-fopenmp"                     \
-      -DFFLAGS="-O3"                                \
-      -DLDFLAGS=""                                  \
-      -DNX=200 \
-      -DNZ=100 \
-      -DSIM_TIME=1000 \
+cmake -DCMAKE_Fortran_COMPILER=mpif90                                     \
+      -DOPENMP_FLAGS="-fopenmp"                                           \
+      -DOPENACC_FLAGS="-fopenacc"                                         \
+      -DOPENMP45_FLAGS="-fopenmp"                                         \
+      -DFFLAGS="-O3 -ffree-line-length-none -I/usr/lib/x86_64-linux-gnu/fortran/gfortran-mod-15"  \
+      -DLDFLAGS="-L/usr/lib/x86_64-linux-gnu -lpnetcdf"                   \
+      -DNX=200                                                            \
+      -DNZ=100                                                            \
+      -DSIM_TIME=1000                                                     \
       ..
 
--- fortran/build/cmake_summit_pgi.sh
+++ fortran/build/cmake_summit_pgi.sh
@@ -8,16 +8,15 @@ export TEST_MPI_COMMAND="jsrun -n 1 -c 1 -a 1 -g 1"
 
 ./cmake_clean.sh
 
-cmake -DCMAKE_Fortran_COMPILER=mpif90               \
-      -DPNETCDF_PATH=${OLCF_PARALLEL_NETCDF_ROOT}   \
-      -DOPENMP_FLAGS=-mp                            \
-      -DOPENACC_FLAGS="-ta=nvidia,cc70,ptxinfo"     \
-      -DDO_CONCURRENT_FLAGS="-stdpar=gpu -Minfo=stdpar -gpu=cc70"     \
-      -DFFLAGS="-O3"                                \
-      -DLDFLAGS=""                                  \
-      -DNX=200 \
-      -DNZ=100 \
-      -DDATA_SPEC="DATA_SPEC_GRAVITY_WAVES" \
-      -DSIM_TIME=1000 \
+cmake -DCMAKE_Fortran_COMPILER=mpif90                                                                  \
+      -DFFLAGS="-O3 -Mvect -Mextend -DNO_INFORM -I${OLCF_PARALLEL_NETCDF_ROOT}/include"                \
+      -DLDFLAGS="-L${OLCF_PARALLEL_NETCDF_ROOT}/lib -lpnetcdf"                                         \
+      -DOPENMP_FLAGS="-Minfo=mp"                                                                       \
+      -DOPENACC_FLAGS:STRING="-acc -gpu=cc70,fastmath,loadcache:L1,ptxinfo -Minfo=accel"               \
+      -DOPENMP45_FLAGS="-Minfo=mp -mp=gpu -gpu=cc70,fastmath,loadcache:L1,ptxinfo"                     \
+      -DDO_CONCURRENT_FLAGS:STRING="-stdpar=gpu -Minfo=stdpar -gpu=cc70,fastmath,loadcache:L1,ptxinfo" \
+      -DNX=200                                                                                         \
+      -DNZ=100                                                                                         \
+      -DSIM_TIME=1000                                                                                  \
+      -DOUT_FREQ=2000                                                                                  \
       ..
-
