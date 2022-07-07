80fb434af151442c2ccc074de4e7b6917cae1a91
Matt Norman <normanmr@ornl.gov>
2022-07-07 09:54:01 -0400

    Further cmake file cleanup

--- fortran/build/cmake_summit_gnu.sh
+++ fortran/build/cmake_summit_gnu.sh
@@ -2,22 +2,21 @@
 
 source ${MODULESHOME}/init/bash
 module purge
-module load DefApps gcc/9.3.0 cuda parallel-netcdf cmake
+module load DefApps gcc/11.2.0 cuda parallel-netcdf cmake
 
 export TEST_MPI_COMMAND="jsrun -n 1 -c 1 -a 1 -g 1"
 
 ./cmake_clean.sh
 
-cmake -DCMAKE_Fortran_COMPILER=mpif90               \
-      -DPNETCDF_PATH=${OLCF_PARALLEL_NETCDF_ROOT}   \
-      -DOPENACC_FLAGS="-fopenacc -foffload=\"-lm -O3\" " \
-      -DOPENMP_FLAGS="-fopenmp"                     \
-      -DOPENMP45_FLAGS="-fopenmp -foffload=\"-lm -O3\" " \
-      -DFFLAGS="-O3"                                \
-      -DLDFLAGS=""                                  \
-      -DNX=200 \
-      -DNZ=100 \
-      -DDATA_SPEC="DATA_SPEC_GRAVITY_WAVES" \
-      -DSIM_TIME=1000 \
+cmake -DCMAKE_Fortran_COMPILER=mpif90                                                             \
+      -DFFLAGS="-O3 -ffree-line-length-none -I${OLCF_PARALLEL_NETCDF_ROOT}/include" \
+      -DLDFLAGS="-L${OLCF_PARALLEL_NETCDF_ROOT}/lib -lpnetcdf"                                    \
+      -DOPENACC_FLAGS="-fopenacc -foffload=\"-lm -latomic\""                                           \
+      -DOPENMP_FLAGS="-fopenmp"                                                                   \
+      -DOPENMP45_FLAGS="-fopenmp -foffload=\"-lm -latomic\""                                           \
+      -DNX=200                                                                                    \
+      -DNZ=100                                                                                    \
+      -DDATA_SPEC="DATA_SPEC_GRAVITY_WAVES"                                                       \
+      -DSIM_TIME=1000                                                                             \
       ..
 
--- fortran/build/cmake_summit_ibm.sh
+++ fortran/build/cmake_summit_ibm.sh
@@ -8,15 +8,13 @@ export TEST_MPI_COMMAND="jsrun -n 1 -c 1 -a 1 -g 1"
 
 ./cmake_clean.sh
 
-cmake -DCMAKE_Fortran_COMPILER=mpif90               \
-      -DPNETCDF_PATH=${OLCF_PARALLEL_NETCDF_ROOT}   \
-      -DOPENMP_FLAGS="-qsmp=omp"                    \
-      -DOPENMP45_FLAGS="-qsmp=omp -qoffload"        \
-      -DFFLAGS="-O3"                                \
-      -DLDFLAGS=""                                  \
-      -DNX=200 \
-      -DNZ=100 \
-      -DDATA_SPEC="DATA_SPEC_GRAVITY_WAVES" \
-      -DSIM_TIME=1000 \
+cmake -DCMAKE_Fortran_COMPILER=mpif90                          \
+      -DFFLAGS="-O3 -I${OLCF_PARALLEL_NETCDF_ROOT}/include"    \
+      -DLDFLAGS="-L${OLCF_PARALLEL_NETCDF_ROOT}/lib -lpnetcdf" \
+      -DOPENMP_FLAGS="-qsmp=omp"                               \
+      -DOPENMP45_FLAGS="-qsmp=omp -qoffload"                   \
+      -DNX=200                                                 \
+      -DNZ=100                                                 \
+      -DDATA_SPEC="DATA_SPEC_GRAVITY_WAVES"                    \
+      -DSIM_TIME=1000                                          \
       ..
-
