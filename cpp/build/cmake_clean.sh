cd76bdff8b86d865d19227e8a0840e93654adb7a
Matt Norman <normanmr@ornl.gov>
2022-07-13 11:26:44 -0400

    Adding new z-dim simd to C++ YAKL

--- cpp/build/cmake_clean.sh
+++ cpp/build/cmake_clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm -rf CMakeCache.txt CMakeFiles CTestTestfile.cmake Makefile cmake_install.cmake Testing mpi openacc \
+rm -rf CMakeCache.txt CMakeFiles CTestTestfile.cmake Makefile cmake_install.cmake Testing mpi openacc    \
        openmp serial openmp45 mpi_test openacc_test openmp_test serial_test openmp45_test YAKL output.nc \
-       parallelfor parallelfor_test parallelfor_simd parallelfor_simd_test
+       parallelfor parallelfor_test parallelfor_simd_x parallelfor_simd_x_test parallelfor_simd_z        \
+       parallelfor_simd_z_test
 
--- fortran/build/cmake_thatchroof_gnu.sh
+++ fortran/build/cmake_thatchroof_gnu.sh
@@ -12,8 +12,8 @@ export OMPI_FC=gfortran
 
 cmake -DCMAKE_Fortran_COMPILER=mpif90                                     \
       -DOPENMP_FLAGS="-fopenmp"                                           \
-      -DOPENACC_FLAGS="-fopenacc -ffast-math -foffload=nvptx-none=\"-lm -O3 -ffast-math -DSINGLE_PREC -march=sm_80 -moptimize\" -fopenacc-dim=16384:1:128 -DSINGLE_PREC -fopt-info-omp"                                         \
-      -DOPENMP45_FLAGS="-fopenmp -ffast-math -foffload=nvptx-none=\"-lm -O3 -latomic -ffast-math -DSINGLE_PREC -march=sm_80 -moptimize\" -DSINGLE_PREC -fopt-info-omp"                                         \
+      -DOPENACC_FLAGS="-fopenacc -ffast-math -foffload=nvptx-none=\"-lm -O3 -ffast-math -DSINGLE_PREC -march=sm_80 -moptimize\" -fopenacc-dim=16384:1:128 -DSINGLE_PREC -fopt-info-omp" \
+      -DOPENMP45_FLAGS="-fopenmp -ffast-math -foffload=nvptx-none=\"-lm -O3 -latomic -ffast-math -DSINGLE_PREC -march=sm_80 -moptimize\" -DSINGLE_PREC -fopt-info-omp"                  \
       -DFFLAGS="-O3 -march=native -mtune=native -ffree-line-length-none -DNO_INFORM -DSINGLE_PREC -I/usr/lib/x86_64-linux-gnu/fortran/gfortran-mod-15"  \
       -DLDFLAGS="-L/usr/lib/x86_64-linux-gnu -lpnetcdf"                   \
       -DNX=2048                                                           \
