fed1d50b4f03df3a0907892aee07f461b57ea74e
Matt Norman <normanmr@ornl.gov>
2023-03-10 12:19:02 -0500

    Updating gnu c files for summit and thatchroof

--- c/build/cmake_summit_gnu.sh
+++ c/build/cmake_summit_gnu.sh
@@ -10,10 +10,9 @@ export TEST_MPI_COMMAND="jsrun -n 1 -c 1 -a 1 -g 1"
 
 cmake -DCMAKE_CXX_COMPILER=mpic++                                                     \
       -DCXXFLAGS="-O3 -std=c++11 -I${OLCF_PARALLEL_NETCDF_ROOT}/include"   \
+      -DOPENACC_FLAGS="-fopenacc -foffload=nvptx-none=\"-lm -Ofast -ffast-math -march=sm_70 -moptimize\" -fopenacc-dim=16384:1:128 -fopt-info-omp" \
+      -DOPENMP45_FLAGS="-fopenmp -foffload=nvptx-none=\"-lm -Ofast -latomic -ffast-math -march=sm_70 -moptimize\" -fopt-info-omp"                  \
       -DLDFLAGS="-L${OLCF_PARALLEL_NETCDF_ROOT}/lib -lpnetcdf"                        \
-      -DOPENACC_FLAGS="-fopenacc -foffload=\"-lm -latomic\""                          \
-      -DOPENMP_FLAGS="-fopenmp"                                                       \
-      -DOPENMP45_FLAGS="-fopenmp -foffload=\"-lm -latomic\""                          \
       -DNX=200                                                                        \
       -DNZ=100                                                                        \
       -DDATA_SPEC="DATA_SPEC_GRAVITY_WAVES"                                           \
--- c/build/cmake_thatchroof_gnu.sh
+++ c/build/cmake_thatchroof_gnu.sh
@@ -11,10 +11,10 @@ export OMPI_CXX=g++
 ./cmake_clean.sh
 
 cmake -DCMAKE_CXX_COMPILER=mpic++                                         \
+      -DCXXFLAGS="-O3 -march=native -std=c++11"                           \
       -DOPENMP_FLAGS="-fopenmp"                                           \
       -DOPENACC_FLAGS="-fopenacc -foffload=nvptx-none=\"-lm -Ofast -ffast-math -march=sm_80 -moptimize\" -fopenacc-dim=16384:1:128 -fopt-info-omp" \
       -DOPENMP45_FLAGS="-fopenmp -foffload=nvptx-none=\"-lm -Ofast -latomic -ffast-math -march=sm_80 -moptimize\" -fopt-info-omp"                  \
-      -DCXXFLAGS="-O3 -march=native -std=c++11"  \
       -DLDFLAGS="-L/usr/lib/x86_64-linux-gnu -lpnetcdf"                   \
       -DNX=200                                                            \
       -DNZ=100                                                            \
