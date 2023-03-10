d1e8c4339797c738f216fb489c519ef0757a1dba
Matt Norman <normanmr@ornl.gov>
2023-03-10 11:38:04 -0500

    crusher files working again

--- cpp/build/cmake_crusher_amd_cpu.sh
+++ cpp/build/cmake_crusher_amd_cpu.sh
@@ -3,14 +3,12 @@
 source ${MODULESHOME}/init/bash
 module load PrgEnv-amd cray-parallel-netcdf cmake craype-accel-amd-gfx90a
 
-export TEST_MPI_COMMAND="mpirun -n 1"
+export TEST_MPI_COMMAND=""
 unset CXX
 unset CC
 unset FC
 unset CUDAFLAGS
 unset CXXFLAGS
-
-export MPICH_GPU_SUPPORT_ENABLED=1
 
 ./cmake_clean.sh
 
@@ -19,7 +17,7 @@ cmake -DCMAKE_CXX_COMPILER=CC                       \
       -DCMAKE_Fortran_COMPILER=ftn                  \
       -DYAKL_ARCH=""                                \
       -DYAKL_CXX_FLAGS="-DNO_INFORM -DGPU_AWARE_MPI -O3 -ffast-math -I${PNETCDF_DIR}/include" \
-      -DLDFLAGS="-L${OLCF_PARALLEL_NETCDF_ROOT}/lib -lpnetcdf"  \
+      -DLDFLAGS="-L${PNETCDF_DIR}/lib -lpnetcdf"  \
       -DNX=16384                                    \
       -DNZ=8192                                     \
       -DSIM_TIME=0.1                                \
--- cpp/build/cmake_crusher_amd_gpu.sh
+++ cpp/build/cmake_crusher_amd_gpu.sh
@@ -3,7 +3,7 @@
 source ${MODULESHOME}/init/bash
 module load PrgEnv-amd cray-parallel-netcdf cmake craype-accel-amd-gfx90a
 
-export TEST_MPI_COMMAND="mpirun -n 1"
+export TEST_MPI_COMMAND=""
 unset CXX
 unset CC
 unset FC
@@ -18,7 +18,7 @@ cmake -DCMAKE_CXX_COMPILER=CC                       \
       -DCMAKE_C_COMPILER=cc                         \
       -DCMAKE_Fortran_COMPILER=ftn                  \
       -DYAKL_ARCH="HIP"                             \
-      -DYAKL_HIP_FLAGS="-DNO_INFORM -DGPU_AWARE_MPI -O3 -ffast-math -D__HIP_ROCclr__ -D__HIP_ARCH_GFX90A__=1 --rocm-path=${ROCM_PATH} --offload-arch=gfx90a -x hip -I${PNETCDF_DIR}/include" \
+      -DYAKL_HIP_FLAGS="-DNO_INFORM -DGPU_AWARE_MPI -O3 -ffast-math -Wno-unused-result -D__HIP_ROCclr__ -D__HIP_ARCH_GFX90A__=1 --rocm-path=${ROCM_PATH} --offload-arch=gfx90a -x hip -I${PNETCDF_DIR}/include" \
       -DLDFLAGS="-L${PNETCDF_DIR}/lib -lpnetcdf --rocm-path=${ROCM_PATH} -L${ROCM_PATH}/lib -lamdhip64" \
       -DNX=1024                                     \
       -DNZ=512                                      \
